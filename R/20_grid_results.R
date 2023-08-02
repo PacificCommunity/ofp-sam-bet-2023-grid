86aff8079fafd17295a7f768f65707725bac1452
Jemery Day <jemeryd@spc.int>
2023-08-02 23:34:27 +0000

    Full list of grid files

--- R/20_grid_results.R
+++ R/20_grid_results.R
@@ -9,6 +9,9 @@ folders <- c("d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a050_h065",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a075_h065", 
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a075_h080",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a075_h095",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a100_h065", 
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a100_h080",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a100_h095",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a050_h065", 
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a050_h080",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a050_h095",
@@ -17,7 +20,16 @@ folders <- c("d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a050_h065",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a075_h095",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a100_h065", 
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a100_h080",
-             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a100_h095")
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s20_a100_h095",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a050_h065", 
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a050_h080",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a050_h095",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a075_h065", 
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a075_h080",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a075_h095",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a100_h065", 
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a100_h080",
+             "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a100_h095")
 
 
 
