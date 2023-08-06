f2f635f2c153360ae39be542e7aa8b872e75bab2
Jemery Day <jemeryd@spc.int>
2023-08-06 01:40:45 +0000

    Add Mix1 directories for results summary

--- R/20_grid_results.R
+++ R/20_grid_results.R
@@ -32,11 +32,24 @@ folders <- c("d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s10_a050_h065",
              "d:/bet2023GridOutput/jitterMix2Grid/Jitter_grid_m2_s40_a100_h095")
 
 
+folders <- c("d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s10_a050_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s10_a075_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s10_a100_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s20_a050_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s20_a075_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s20_a100_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s40_a050_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s40_a075_h80",
+             "d:/bet2023GridOutput/jitterMix1Grid/Jitter_14f_par_m1_s40_a100_h80")
+
+
+
 
 grid.list <- lapply(folders, grid_results)
 grid.tab <- do.call(rbind, grid.list)
 
 ################################################################################
 
-write.xlsx(grid.tab, "../grid_results.xlsx")
+# write.xlsx(grid.tab, "../Mix2grid_results.xlsx")
+write.xlsx(grid.tab, "../Mix1grid_results.xlsx")
 cat("\nWrote grid_results.xlsx\n")
